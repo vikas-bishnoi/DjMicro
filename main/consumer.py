1f81f6e9464e05a2e483369b1fde021e72e24781
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-20 01:11:53 +0530

    Update dockerfiles to run consumers in a different container

--- main/consumer.py
+++ main/consumer.py
@@ -11,7 +11,7 @@ def callback(channel, method, properties, body):
     print('Received in main')
     print(body)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print("Start consuming")
 
