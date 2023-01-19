606e53e165673d2854cb323364506c03fd77fda8
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-20 00:36:28 +0530

    Add models and migrate

--- main/main.py
+++ main/main.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db/main"
-CORS(app)
-
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+CORS(app)
 
 
 class Product(db.Model):
@@ -24,6 +28,8 @@ class ProductUser(db.Model):
 @app.route('/')
 def index():
     return "hello"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
