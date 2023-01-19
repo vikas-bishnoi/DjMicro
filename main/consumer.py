266c79661d9c1df7fd616db1c29dfdc5c5d78c4d
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-20 02:30:58 +0530

    Add appcontext on saving data to database

--- main/consumer.py
+++ main/consumer.py
@@ -1,6 +1,6 @@
 import pika, json
 
-from main import Product, db
+from main import Product, app, db
 
 
 params = pika.URLParameters('amqps://olrndrtz:ik9NeulCSDzVovbpx4O8g6s7Ty6BPjG9@puffin.rmq2.cloudamqp.com/olrndrtz')
@@ -13,28 +13,31 @@ channel.queue_declare(queue='main')
 def callback(channel, method, properties, body):
     print('Received in main')
     data = json.loads(body)
-    print(data)
     
     if properties.content_type == 'product_created':
         product = Product(id=data['id'], title=data['title'], image=data['image'])
-        db.session.add(product)
-        db.session.commit()
+        with app.app_context():
+            db.session.add(product)
+            print('Created a new product')
+            db.session.commit()
     
     elif properties.content_type == 'product_updated':
         product = Product.query.get(data['id'])
         product.title = data['title']
         product.image = data['image']
-        db.session.commit()
+        with app.app_context():
+            print('updated product')
+            db.session.commit()
     
     elif properties.content_type == 'product_deleted':
         product = Product.query.get(data['id'])
-        db.session.delete(product)
-        db.session.commit()
+        with app.app_context():
+            db.session.delete(product)
+            print('deleted product')
+            db.session.commit()
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print("Start consuming")
-
 channel.start_consuming()
-
 channel.close()
