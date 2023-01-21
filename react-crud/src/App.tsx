dc0a9ab22478b893fde9f932dc3d10e26e66e875
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-22 01:18:51 +0530

    Add products component

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -3,38 +3,18 @@ import "./App.css";
 
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import Products from "./admin/Products";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container-fluid">
-        <Menu />
         <div className="row">
+          <Menu />
+
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <h2>Section title</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Products />
           </main>
         </div>
       </div>
