5946f47d94aacf9901c756fb86ffc3f66e9d4776
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-22 05:57:14 +0530

    Add routing for application

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import Products from "./admin/Products";
@@ -14,7 +16,11 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Products />
+            <Router>
+              <Routes>
+                <Route path="admin/products" element={<Products />} />
+              </Routes>
+            </Router>
           </main>
         </div>
       </div>
