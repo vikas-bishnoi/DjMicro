1050221b8de1ca28c2ba60c7f4a564e79a9547b5
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-23 18:28:18 +0530

    Added create product functionality

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'products',
     'corsheaders',
-    'products'
 ]
 
 MIDDLEWARE = [
--- react-crud/src/admin/ProductCreate.tsx
+++ react-crud/src/admin/ProductCreate.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { SyntheticEvent, useState } from "react";
+import { redirect } from "react-router-dom";
 import Wrapper from "./Wrapper";
 
 const ProductCreate = () => {
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
 
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(title, image);
+    await fetch("http://localhost:8000/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        image,
+      }),
+    });
+    redirect("/admin/products");
+  };
+
   return (
     <Wrapper>
-      <form onSubmit={() => {}}>
+      <form onSubmit={submit}>
         <div className="form-group">
           <label>Title</label>
           <input
