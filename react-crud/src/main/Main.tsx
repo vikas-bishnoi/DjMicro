f0f8d064e70af0932b55412f752c9f58146d1a24
Vikas Bishnoi <vikaskbm@gmail.com>
2023-01-23 20:35:42 +0530

    Display product in home page

--- react-crud/src/main/Main.tsx
+++ react-crud/src/main/Main.tsx
@@ -1,40 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Product } from "../interfaces/product";
 
 const Main = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch("http://localhost:8001/api/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
   return (
     <>
       <main>
         <div className="album py-5 bg-light">
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div className="col">
-                <div className="card shadow-sm">
-                  <div className="card-body">
-                    <p className="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
+              {products.map((product: Product) => {
+                return (
+                  <div className="col">
+                    <div className="card shadow-sm">
+                      <img src={product.image} height="180" alt="" />
+                      <div className="card-body">
+                        <p className="card-text">{product.title}</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                          <div className="btn-group">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-outline-secondary"
+                            >
+                              Like
+                            </button>
+                          </div>
+                          <small className="text-muted">
+                            {product.likes} Likes
+                          </small>
+                        </div>
                       </div>
-                      <small className="text-muted">9 mins</small>
                     </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </div>
         </div>
